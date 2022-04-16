6388a705cf5d38226bb09467368f38d7dbfea451
Md. Shanjid Hasan Abir <100505657+MdShanjidHasanAbir@users.noreply.github.com>
2022-04-16 19:16:05 +0600

    Update Digital Calculator.cpp

--- Digital Calculator.cpp
+++ Digital Calculator.cpp
@@ -1,1 +1,23 @@
 Project Digital Calculator
+//find the factorial of a number..
+#include<iostream>
+using namespace std;
+int main()
+{
+int num,factorial=1;
+cout<<"enter the number=";
+cin>>num ;
+if(num<0)
+cout<<"Math erorr";
+else if (num<=1)
+cout<<num<<"! ="<<factorial;
+else
+{
+for(int x= num; counter >= 2; x--)
+{
+factorial=factorial*x;
+}
+cout<<"The factorial of "<< num<< "(!)"<< "is="<<factorial;
+}
+return 0;
+}
