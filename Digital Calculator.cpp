6fd44750e5408ccd6801ac321733a8a620b5dd3c
Jahirul islam Nahid <100505577+jahirulislamnahid@users.noreply.github.com>
2022-04-17 00:09:58 +0600

    Update Digital Calculator.cpp

--- Digital Calculator.cpp
+++ Digital Calculator.cpp
@@ -1,4 +1,27 @@
 Project Digital Calculator
+//find the average of n integer number.
+void average(void)
+{
+{
+system("CLS");
+float arra[10000],sum=0.0;
+int n;
+cout<<"Enter the amount "<<endl;
+cin>>n;
+system("CLS");
+for(int i=0;i<n;i++)
+{
+cout<<"Enter the value of "<<i+1<<" : ";
+cin>>arra[i];
+cout<<endl;
+}
+for(int i=0;i<n;i++)
+{
+sum+=arra[i];
+}
+cout<<"The average is : "<<sum/n;
+}
+}
 //=------------------------------ my part
 
 //vector
