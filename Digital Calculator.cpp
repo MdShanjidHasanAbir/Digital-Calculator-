84496e626666a1bcf945cbb94faef3bcd3c1b0f4
ahmedseum <100505704+ahmedseum@users.noreply.github.com>
2022-04-17 00:07:41 +0600

    Update Digital Calculator.cpp

--- Digital Calculator.cpp
+++ Digital Calculator.cpp
@@ -1,4 +1,90 @@
 Project Digital Calculator
+//=------------------------------ my part
+
+//vector
+
+
+
+void vect(void)
+{
+
+float a,b,c,sum;
+int dimention;
+system("CLS");
+cout<<"Enter the dimention "<<endl;
+cin>>dimention;
+switch(dimention)
+{
+case 2:
+system("CLS");
+cout<<"a :- ";
+cin>>a;
+cout<<endl;
+cout<<endl;
+cout<<"b :- ";
+cin>>b;
+sum=pow((a*a+b*b),.5);
+cout<<"The magnitude of your vector ";
+cout<<sum;
+break;
+case 3:
+system("CLS");
+cout<<"a :- ";
+cin>>a;
+cout<<endl;
+cout<<endl;
+cout<<"b :- ";
+cin>>b;
+cout<<endl;
+cout<<endl;
+cout<<"c :- ";
+cin>>c;
+cout<<endl;
+cout<<endl;
+sum=pow((a*a+b*b+c*c),.5);
+cout<<"The magnitude of your vector : ";
+cout<<sum;
+break;
+default:
+cout<<"Can't calculate "<<endl;
+break;
+}
+}
+//matrix
+void matri(void)
+{
+
+float arra2d[10][10];
+int dis,row,col;
+cout<<" welcome to the matrix "<<endl;
+cout<<"Enter the amount of row : ";cin>>row;
+cout<<"Enter the amount of column : ";cin>>col;
+for(int i=0;i<row;i++)
+{
+for(int j=0;j<col;j++)
+{
+cin>>arra2d[i][j];
+cout<<endl;
+}
+
+
+
+}
+cout <<"The matrix will be"<<endl;
+for(int i=0;i<row;i++)
+{
+for(int j=0;j<col;j++)
+{
+cout<<"a ["<<i+1<<"]["<<j+1<<"] :";
+cout<<arra2d[i][j];
+cout<<endl;
+}}}
+
+//--------------------------------
+
+
+
+
 // find the factorial of a number.
 void factorial(void)
 {
